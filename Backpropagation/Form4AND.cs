59f879cdaac54d4c1877a5ba15c2b097c1be2e90
friamae <camellofriamae@gmail.com>
2024-12-06 23:41:40 +0800

    -updated epoch

--- Backpropagation/Form4AND.cs
+++ Backpropagation/Form4AND.cs
@@ -16,7 +16,7 @@ namespace Backpropagation
 
         NeuralNet nn;
         int minHiddenLayers = 100;
-        int minEpochs = 100;
+        int minEpochs = 5120;
 
         public _4AND()
         {
